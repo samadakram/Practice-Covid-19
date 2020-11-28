6fbc8ea5830fed05a81cd9b0c238a428909aec77
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-11-29 02:22:01 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 // Import Components
-import { Cards, Chart, CountryPicker } from './components';
+import Cards from './components/Cards/Cards';
+import Chart from './components/Chart/Chart';
+import CountryPicker from './components/CountryPicker/CountryPicker';
 import styles from './App.module.css';
 
 function App() {
