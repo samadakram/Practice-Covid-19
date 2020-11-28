01d608fac5dd50e25018943d8ebd3b5bf9f8bc4c
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-11-29 03:07:29 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,15 +5,25 @@ import Cards from './components/Cards/Cards';
 import Chart from './components/Chart/Chart';
 import CountryPicker from './components/CountryPicker/CountryPicker';
 import styles from './App.module.css';
+import { fetchData } from './api';
 
-function App() {
-  return (
-    <div className={styles.container}>
-      <Cards />
-      <CountryPicker />
-      <Chart />
-    </div>
-  );
+class App extends React.Component {
+
+  async componentDidMount() {
+    const data = await fetchData();
+
+    console.log(data);
+  }
+
+  render () {
+    return (
+      <div className={styles.container}>
+        <Cards />
+        <CountryPicker />
+        <Chart />
+      </div>
+    )
+  }
 }
 
 export default App;
