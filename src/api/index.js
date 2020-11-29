d7c083df890d417f0b3dcf9303129d5475b50d2d
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-11-30 03:49:33 +0500

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -13,7 +13,7 @@ export const fetchData = async () => {
     }
 }
 
-const fetchDailyData = async () => {
+export const fetchDailyData = async () => {
     try {
         const {data} = await axios.get(`${url}/daily`);
         console.log(data);
