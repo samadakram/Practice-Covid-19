04ec46cd3802ee9adc983bc710cf0d915ecb4706
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-12-01 03:30:54 +0500

    Update CountryPicker.jsx

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -9,19 +9,19 @@ const CountryPicker = () => {
 
     const [fetchedCountries, setFetchedCountries] = useState([]);
 
-    useEffect(()=> {
+    useEffect(() => {
         const fetchAPI = async () => {
             setFetchedCountries(await fetchCountries());
         }
 
         fetchAPI();
-    },[setFetchedCountries]);
-    console.log(fetchedCountries);
+    }, [setFetchedCountries]);
     return (
         <div>
             <FormControl className={styles.formControl}>
                 <NativeSelect>
                     <option value="global">Global</option>
+                    {fetchedCountries.map((country, i) => <option key={i} value={country}>{country}</option>)}
                 </NativeSelect>
             </FormControl>
         </div>
