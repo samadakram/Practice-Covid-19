3f325b02cc016059499bea719d6b8d021dfa3cfa
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-12-01 03:25:52 +0500

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -4,19 +4,19 @@ const url = 'https://covid19.mathdro.id/api';
 
 export const fetchData = async () => {
     try {
-        const {data: {confirmed, recovered, deaths, lastUpdate} } = await axios.get(url);
+        const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(url);
 
-        return { confirmed, recovered, deaths, lastUpdate} ;
+        return { confirmed, recovered, deaths, lastUpdate };
 
     } catch (error) {
-        
+
     }
 }
 
 export const fetchDailyData = async () => {
     try {
-        const {data} = await axios.get(`${url}/daily`);
-        const modifiedData = data.map((dailyData)=> ({
+        const { data } = await axios.get(`${url}/daily`);
+        const modifiedData = data.map((dailyData) => ({
             confirmed: dailyData.confirmed.total,
             deaths: dailyData.deaths.total,
             date: dailyData.reportDate,
@@ -28,12 +28,12 @@ export const fetchDailyData = async () => {
     }
 }
 
-export const countries = async () => {
+export const fetchCountries = async () => {
     try {
-        const response = await axios.get(`${url}/countries`);
+        const { data: { countries } } = await axios.get(`${url}/countries`);
 
-        console.log(response);
+        return countries.map((country)=> country.name);
     } catch (error) {
-        
+        console.log(error);
     }
 }
